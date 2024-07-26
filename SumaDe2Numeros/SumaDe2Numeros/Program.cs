9934009914b61d549115af732e343114c0e511ac
Emil Alexis Ramos Morales <USSS019624@ugb.edu.sv>
2024-07-26 16:28:35 -0600

    SUMA DE DECIMALES DE DOS NUMEROS

---
+++ SumaDe2Numeros/SumaDe2Numeros/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SumaDe2Numeros
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //SUMA DE DECIMALES DE DOS NUMEROS
+
+            Console.Write("num 1:");
+            double num1 = double.Parse(Console.ReadLine()); //"5" ->
+
+            Console.Write("num 2:");
+            double num2 = double.Parse(Console.ReadLine());
+
+            double respuesta = num1 + num2;
+            Console.WriteLine("La suma de {0} + {1} = {2}", num1, num2, respuesta);
+
+            //Pause
+            Console.ReadLine();
+
+        }
+    }
+}
